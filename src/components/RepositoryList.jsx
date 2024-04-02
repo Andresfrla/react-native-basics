b2dcecf33166d58b268ff898b4a2e59647a44a30
Andresfrla <125943749+Andresfrla@users.noreply.github.com>
2024-04-02 10:41:18 -0600

    adding the list and rendering styles

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,23 +1,18 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { FlatList, Text, View } from 'react-native'
 import repositories from '../data/repositories'
+import RepositoryItem from './RepositoryItem'
 
 const RepositoryList = () => {
   return (
-    <View>
-      {repositories.map(repo => (
-        <View key={repo.id}>
-            <Text>id: {repo.id}</Text>
-            <Text>FullName: {repo.fullName}</Text>
-            <Text>Description: {repo.description}</Text>
-            <Text>Language: {repo.language}</Text>
-            <Text>Starts: {repo.stargazersCount}</Text>
-            <Text>Forks: {repo.forksCount}</Text>
-            <Text>Review: {repo.reviewCount}</Text>
-            <Text>Rating: {repo.ratingAverage}</Text>
-        </View>
-      ))}
-    </View>
+    <FlatList 
+        data={repositories}
+        ItemSeparatorComponent={() => <Text></Text>}
+        renderItem={({ item: repo }) => (
+            <RepositoryItem {...repo}/>
+        )}
+    >
+    </FlatList>
   )
 }
 
