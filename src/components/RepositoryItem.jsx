b56ad52de70a4dbc4242ac36da2dae8c15723ad8
Andresfrla <125943749+Andresfrla@users.noreply.github.com>
2024-04-02 10:56:44 -0600

    create a theme

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,17 +1,26 @@
 import React from "react"
-import { Text, View } from "react-native"
+import { StyleSheet, Text, View } from "react-native"
+import { StyleText } from "./StyleText"
 
 const RepositoryItem = (props) => (
-    <View key={props.id} style={{padding: 20, paddingBottom: 5, paddingTop: 5}}>
-        <Text style={{ fontWeight: 'bold', marginBottom: 5 }}>id: {props.id}</Text>
-        <Text>FullName: {props.fullName}</Text>
-        <Text>Description: {props.description}</Text>
-        <Text>Language: {props.language}</Text>
-        <Text>Starts: {props.stargazersCount}</Text>
-        <Text>Forks: {props.forksCount}</Text>
-        <Text>Review: {props.reviewCount}</Text>
-        <Text>Rating: {props.ratingAverage}</Text>
+    <View key={props.id} style={styles.container}>
+        <StyleText big bold>id: {props.id}</StyleText>
+        <StyleText blue>FullName: {props.fullName}</StyleText>
+        <StyleText bold>Description: {props.description}</StyleText>
+        <StyleText blue>Language: {props.language}</StyleText>
+        <StyleText small>Starts: {props.stargazersCount}</StyleText>
+        <StyleText small>Forks: {props.forksCount}</StyleText>
+        <StyleText small>Review: {props.reviewCount}</StyleText>
+        <StyleText small>Rating: {props.ratingAverage}</StyleText>
     </View>
 )
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 20,
+        paddingBottom: 5,
+        paddingTop: 5,
+    },
+})
 
 export default RepositoryItem
---
+++ src/theme.js
@@ -0,0 +1,18 @@
+const theme = {
+    colors: {
+        textPrimary: '#24292e',
+        textSecondary: '#586069',
+        primary: '#0366d6'
+    },
+    fontSizes: {
+        body: 14,
+        sudheading: 16
+    },
+    fonts: {
+        main: 'System'
+    },
+    fontWeights: {
+        normal: '400',
+        bodl: '700'
+    }
+}
