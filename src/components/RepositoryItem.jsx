38a9824e2bfda48f142fc49a04415acfb42060ce
Andresfrla <125943749+Andresfrla@users.noreply.github.com>
2024-04-02 12:14:17 -0600

    styling the cards

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,14 +1,25 @@
 import React from "react"
-import { StyleSheet, Text, View } from "react-native"
+import { Image, StyleSheet, Text, View } from "react-native"
 import { StyleText } from "./StyleText"
 import RepositoryStats from "./RepositoryStats"
 import theme from "../theme"
 
+const RepositoryItemHeader = (props) => (
+        <View style={{ flexDirection: "row", paddingBottom: 2}}>
+            <View style={{ flex: 0, paddingRight: 10 }}>
+                <Image style={styles.image} source={{ uri: props.ownerAvatarUrl }}/>
+            </View>
+            <View style={{ flex: 1 }}>
+                <StyleText fontWeight='bold'>FullName: {props.fullName}</StyleText>
+                <StyleText color='secondary'>{props.description}</StyleText>
+                <StyleText style={styles.language}>{props.language}</StyleText>
+            </View>
+        </View>
+    )
+
 const RepositoryItem = (props) => (
     <View key={props.id} style={styles.container}>
-        <StyleText fontSize='subheading' fontWeight='bold'>FullName: {props.fullName}</StyleText>
-        <StyleText >{props.description}</StyleText>
-        <StyleText style={styles.language}>{props.language}</StyleText>
+        <RepositoryItemHeader {...props} />
         <RepositoryStats {...props}/>
     </View>
 )
@@ -24,8 +35,14 @@ const styles = StyleSheet.create({
         color: theme.colors.white,
         backgroundColor: theme.colors.primary,
         alignSelf: 'flex-start',
-        borderRadius: 4,
+        marginTop: 4,
+        marginVertical: 4,
         overflow: 'hidden',
+    },
+    image: {
+        width: 48,
+        height: 48,
+        borderRadius: 4
     }
 })
 
